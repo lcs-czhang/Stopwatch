203949c13d4c08b21bc7a97becb88a2f2f8b4266
Youzhou Zhang <czhang@lcs.on.ca>
2023-11-03 09:40:56 -0400

    Initial Commit

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Yuzhou Zhang on 2023-11-03.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
